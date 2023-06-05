bc64a2ec4efbc5880498c32122edcc98cc5e690d
Yevhen4ikk <relog0077@gmail.com>
2023-06-05 18:30:18 +0300

    Описание коммита

---
+++ audiobot.py
@@ -0,0 +1,91 @@
+import os
+import logging
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from googleapiclient.discovery import build
+from pydub import AudioSegment
+from pydub.utils import make_chunks
+import urllib.request
+
+# Set up logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Set up Telegram bot
+bot = telegram.Bot(token=os.environ.get('TELEGRAM_TOKEN'))
+
+# Set up YouTube Data API
+api_key = os.environ.get('YOUTUBE_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+# Define command handlers
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I'm a bot that can search for and add songs from YouTube to the playlist. Use the /search command to search for a song.")
+
+def search(update, context):
+    query = ' '.join(context.args)
+    if not query:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter a search query.")
+        return
+    search_response = youtube.search().list(q=query, type='video', part='id,snippet', maxResults=10).execute()
+    videos = []
+    for search_result in search_response.get('items', []):
+        video_id = search_result['id']['videoId']
+        video_title = search_result['snippet']['title']
+        videos.append({'id': video_id, 'title': video_title})
+    if not videos:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="No videos found.")
+        return
+    message = "Search results:\n"
+    for i, video in enumerate(videos):
+        message += f"{i+1}. {video['title']}\n"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def add(update, context):
+    query = ' '.join(context.args)
+    if not query:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Please enter a video ID.")
+        return
+    video_id = query
+    video_response = youtube.videos().list(id=video_id, part='snippet').execute()
+    video_title = video_response['items'][0]['snippet']['title']
+    # Download the video
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    urllib.request.urlretrieve(video_url, 'video.mp4')
+    # Extract the audio
+    audio = AudioSegment.from_file('video.mp4', format='mp4')
+    chunks = make_chunks(audio, 10000)  # Split the audio into 10-second chunks
+    for i, chunk in enumerate(chunks):
+        chunk.export(f'chunk{i}.mp3', format='mp3')  # Export each chunk as an MP3 file
+        # TODO: Add each chunk to the playlist
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Added {video_title} to the playlist.")
+
+def playlist(update, context):
+    # TODO: Retrieve the current playlist and send it to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Playlist:")
+
+def remove(update, context):
+    # TODO: Remove a song from the playlist
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Removed song from playlist.")
+
+def play(update, context):
+    # TODO: Play a song from the playlist
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Playing song.")
+
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+# Set up command handlers
+updater = Updater(token=os.environ.get('TELEGRAM_TOKEN'), use_context=True)
+dispatcher = updater.dispatcher
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('search', search))
+dispatcher.add_handler(CommandHandler('add', add))
+dispatcher.add_handler(CommandHandler('playlist', playlist))
+dispatcher.add_handler(CommandHandler('remove', remove))
+dispatcher.add_handler(CommandHandler('play', play))
+dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+
+# Start the bot
+updater.start_polling()
+updater.idle()
